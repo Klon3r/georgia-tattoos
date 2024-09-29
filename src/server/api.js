5b392d4a1117d8c312d5936a14d03ae03a20ff08
Klon3r <117739313+Klon3r@users.noreply.github.com>
2024-09-29 23:54:43 +1000

    Added error checking with insertBooking()

--- src/server/api.js
+++ src/server/api.js
@@ -2,6 +2,10 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import { insertBooking } from "./database.js";
+
+dotenv.config({ path: "./.env" }); // .env file location
 
 const app = express();
 const port = 3000;
@@ -9,17 +13,39 @@ const port = 3000;
 // Middleware
 app.use(helmet());
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" })); //TODO: Change this on live deployment
+// TODO: Change this on live deployment
+app.use(cors({ origin: "http://192.168.50.173:5173" }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// /booking (POST)
 app.post("/booking", (req, res) => {
-  console.log("Form Data: ", req.body);
-
+  //console.log("Form Data: ", req.body);
   let data = req.body.booking;
-  console.log("Name:", data.firstName);
 
-  // Send back the created status code
-  res.status(201).send();
+  try {
+    insertBooking(
+      data.firstName,
+      data.lastName,
+      data.prefName,
+      data.pronouns,
+      data.email,
+      data.number,
+      data.instagram,
+      data.descTattoo,
+      data.availMonday,
+      data.availTuesday,
+      data.availFriday,
+      data.availSaturday,
+      data.tattooColor,
+      data.workAround
+    );
+    // Send status code
+    res.status(201).send();
+  } catch (err) {
+    // Error
+    console.error("There was an error: ", err);
+    res.status(500).send();
+  }
 });
 
 app.listen(port, () => {
