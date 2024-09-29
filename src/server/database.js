fe8f3e5ecbcf13caec2769c03912419c89a93670
Klon3r <117739313+Klon3r@users.noreply.github.com>
2024-09-29 23:53:32 +1000

    Added deleteTable() & Added to insertBooking()
    - deleteTable(): delete the table from the database.
    - Added pooling information to the insertBooking()

--- src/server/database.js
+++ src/server/database.js
@@ -27,6 +27,7 @@ function createTable() {
     preferred_name VARCHAR(50),
     pronouns VARCHAR(20) NOT NULL,
     email VARCHAR(100) NOT NULL,
+    number VARCHAR(10) NOT NULL, 
     instagram VARCHAR(32) NOT NULL,
     description_of_tattoo TEXT NOT NULL,
     available_monday BOOLEAN DEFAULT FALSE,
@@ -53,6 +54,22 @@ function createTable() {
 }
 
 /**
+ * Delete a specified table in the database if it exists
+ * @param {string} tableName
+ */
+function deleteTable(tableName) {
+  database
+    .connect()
+    .then((client) => {
+      database.query(`DROP TABLE IF EXISTS ${tableName}`);
+      console.log(`${tableName} has been deleted`);
+    })
+    .catch((err) => {
+      console.err("There was an error deleting the table: ", err);
+    });
+}
+
+/**
  * Insert the booking details into the database
  * @param {string} firstName First Name
  * @param {string} lastName Last Name
@@ -68,12 +85,13 @@ function createTable() {
  * @param {string} tattooColor Is it color or black and grey?
  * @param {boolean} workAround Is it working around another tattoo?
  */
-function insertBooking(
+export function insertBooking(
   firstName,
   lastName,
   prefName,
   pronouns,
   email,
+  number,
   instagram,
   descTattoo,
   availMon,
@@ -83,20 +101,37 @@ function insertBooking(
   tattooColor,
   workAround
 ) {
+  const tableName = "booking_test_sept_2024";
+
+  // Connect info for database
+  const database = new Pool({
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    port: process.env.PORT,
+  });
+
+  let client;
+
   database
     .connect()
-    .then((client) => {
+    .then((connectedClient) => {
+      client = connectedClient;
+
       const query = `
         INSERT INTO ${tableName} 
-        (first_name, last_name, preferred_name, pronouns, email, instagram, description_of_tattoo, available_monday, available_tuesday, available_friday, available_saturday, tattoo_color, work_around) 
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13);
+        (first_name, last_name, preferred_name, pronouns, email, number, instagram, description_of_tattoo, available_monday, available_tuesday, available_friday, available_saturday, tattoo_color, work_around) 
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14);
       `;
+
       const values = [
         firstName,
         lastName,
         prefName,
         pronouns,
         email,
+        number,
         instagram,
         descTattoo,
         availMon,
@@ -106,14 +141,17 @@ function insertBooking(
         tattooColor,
         workAround,
       ];
+
       return client.query(query, values);
     })
     .then(() => {
-      console.log(`${firstName} ${lastName} inserted into the database`);
+      console.warn(`Booking: ${firstName} ${lastName} added to the database`);
     })
     .finally(() => {
-      // Release the clients connection
-      client.release();
+      // Release the client connection
+      if (client) {
+        client.release();
+      }
     })
     // ERROR
     .catch((err) => {
@@ -123,20 +161,3 @@ function insertBooking(
       );
     });
 }
-
-// Used for testing
-insertBooking(
-  "Keiran",
-  "Bunyan",
-  "",
-  "He/Him",
-  "test@test.com",
-  "@kbunny21",
-  "This is just a test!",
-  true,
-  true,
-  false,
-  false,
-  "Black & Grey",
-  false
-);
