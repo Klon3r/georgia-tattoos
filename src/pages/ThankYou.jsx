22f3f5df77744502252da2ec897811efef54e6b4
Klon3r <117739313+Klon3r@users.noreply.github.com>
2024-09-29 22:56:39 +1000

    Added Thank You page
    - Used for after a client submits a booking

---
+++ src/pages/ThankYou.jsx
@@ -0,0 +1,22 @@
+function ThankYou() {
+  return (
+    <>
+      <h2>THANK YOU!</h2>
+      <p style={{ marginBottom: "0px" }}>Your booking is now submitted.</p>
+      <p
+        style={{
+          margin: "40px",
+          marginTop: "0px",
+          marginBottom: "0px",
+          textAlign: "center",
+        }}
+      >
+        {" "}
+        Keep an eye on your emails including your junk folder for a response.
+      </p>
+      <p style={{ marginTop: "0px" }}>Looking forward to working with you!</p>
+    </>
+  );
+}
+
+export default ThankYou;
