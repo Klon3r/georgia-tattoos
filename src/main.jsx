8014ece91bce277f608598387022c176e80caaab
Klon3r <117739313+Klon3r@users.noreply.github.com>
2024-09-30 22:06:16 +1000

    Added Error and 403 Error Hash Routes

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,11 @@ import Logo from "./pages/Logo";
 import Homepage from "./pages/Homepage";
 import Booking from "./pages/Booking";
 import Aftercare from "./pages/Aftercare";
-import "./style.css";
 import ThankYou from "./pages/ThankYou";
+import Error from "./pages/Error";
+import Error403 from "./pages/Error403";
+
+import "./style.css";
 
 const router = createHashRouter([
   {
@@ -29,6 +32,16 @@ const router = createHashRouter([
     // Thank You
     path: "thank-you",
     element: <ThankYou />,
+  },
+  {
+    // Error
+    path: "error",
+    element: <Error />,
+  },
+  {
+    // All other routes, 403 Error
+    path: "*",
+    element: <Error403 />,
   },
 ]);
 
