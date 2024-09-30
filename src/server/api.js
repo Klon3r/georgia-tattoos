a68154185ee06fccae7e2bd15301ce359bd0a2dd
Klon3r <117739313+Klon3r@users.noreply.github.com>
2024-10-01 00:30:58 +1000

    Added email sending to API for testing

--- src/server/api.js
+++ src/server/api.js
@@ -4,6 +4,7 @@ import helmet from "helmet";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import { insertBooking } from "./database.js";
+import { sendEmail } from "./email.js";
 
 dotenv.config({ path: "./.env" }); // .env file location
 
@@ -19,28 +20,38 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // /booking (POST)
 app.post("/booking", (req, res) => {
-  //console.log("Form Data: ", req.body);
+  console.log("Form Data: ", req.body);
   let data = req.body.booking;
 
   try {
-    insertBooking(
-      data.firstName,
-      data.lastName,
-      data.prefName,
-      data.pronouns,
-      data.email,
-      data.number,
-      data.instagram,
-      data.descTattoo,
-      data.availMonday,
-      data.availTuesday,
-      data.availFriday,
-      data.availSaturday,
-      data.tattooColor,
-      data.workAround
-    );
+    // insertBooking(
+    //   data.firstName,
+    //   data.lastName,
+    //   data.prefName,
+    //   data.pronouns,
+    //   data.email,
+    //   data.number,
+    //   data.instagram,
+    //   data.descTattoo,
+    //   data.availMonday,
+    //   data.availTuesday,
+    //   data.availFriday,
+    //   data.availSaturday,
+    //   data.locationOnBody,
+    //   data.sizeTattoo
+    //   data.tattooColor,
+    //   data.workAround
+    // );
+
+    try {
+      //sendEmail(process.env.HER_EMAIL, data);
+      sendEmail(process.env.MY_EMAIL, data);
+    } catch (err) {
+      console.error("There has been an error sending the email: ", err);
+    }
+
     // Send status code
-    res.status(201).send();
+    //res.status(201).send();
   } catch (err) {
     // Error
     console.error("There was an error: ", err);
