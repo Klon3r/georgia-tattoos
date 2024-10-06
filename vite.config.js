ad2350696a4d1977fe768a366696857cdde6ec3d
Klon3r <117739313+Klon3r@users.noreply.github.com>
2024-10-06 23:21:50 +1000

    Configured vite settings
    - Added sourcemap: false

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    sourcemap: false, // Ensure source maps are disabled
+  },
+  server: {
+    // Optional: Disable source maps on dev server if needed
+    sourcemap: false,
+  },
+});
