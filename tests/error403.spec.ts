63720a41d96754745c73af8ec6a6a4fbe3b9c7e3
Klon3r <117739313+Klon3r@users.noreply.github.com>
2024-10-17 00:12:52 +1000

    Added testing for error 403

---
+++ tests/error403.spec.ts
@@ -0,0 +1,22 @@
+import { expect, test } from "@playwright/test";
+import { error } from "console";
+
+const error403Page = "localhost:5173#errornotreal";
+
+// Check text
+test("Check error text", async ({ page }) => {
+  await page.goto(error403Page);
+  await expect(
+    page.getByText("That page doesn't exist, please go back to the homepage.")
+  ).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "403 Not Found" })
+  ).toBeVisible();
+});
+
+// Check Button
+test("Check button routes back to homepage", async ({ page }) => {
+  await page.goto(error403Page);
+  await page.getByRole("button", { name: /BACK/i }).click();
+  await expect(page).toHaveURL(/. *#/);
+});
