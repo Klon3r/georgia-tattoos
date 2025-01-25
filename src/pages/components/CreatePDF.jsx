f0e11e30617ed31dd46328a2b13a0e37124b78ce
Klon3r <117739313+Klon3r@users.noreply.github.com>
2025-01-25 21:15:08 +1000

    WIP: Consent Form (Terms & Conditions)

--- src/pages/components/CreatePDF.jsx
+++ src/pages/components/CreatePDF.jsx
@@ -40,7 +40,7 @@ async function CreatePDF(formData) {
     xWidthHalf - imageWidth / 2,
     5,
     imageWidth,
-    50,
+    50
   );
   doc.setFontSize(40);
   const clientHeader = "Client Info";
@@ -65,7 +65,7 @@ async function CreatePDF(formData) {
   y = y + gapSpace;
   // TODO: Add error checking if there is no photo
   doc.text(`License Photo:`, xForData, y);
-  doc.addImage(formData.licensePhoto, "JPEG", xForDataAlign + 1, y - 4, 75, 50);
+  doc.addImage(formData.licensePhoto, "JPEG", xForDataAlign + 1, y - 4, 85, 50);
   let photoSpace = 54;
   y = y + photoSpace;
   doc.text(`Home Address:`, xForData, y);
@@ -139,7 +139,7 @@ async function CreatePDF(formData) {
   doc.text(
     `Are you currently under the influence of drugs or alcohol?`,
     xForData,
-    y,
+    y
   );
   y = y + gapSpace;
   doc.text(`- ${formData.alcoholOrDrugs}`, xForQuestions, y);
@@ -162,8 +162,10 @@ async function CreatePDF(formData) {
   doc.text(`- ${formData.medicalConditions}`, xForQuestions, y, {
     maxWidth: 180,
   });
-  // TODO: If width > 180 then increase y
-  y = y + gapSpace;
+
+  // If the text is too long, then add a new line
+  let textLines = doc.splitTextToSize(formData.medicalConditions, 180);
+  y = y + gapSpace + textLines.length;
 
   if (formData.otherMedicalConditions !== "") {
     doc.text(`Other Medical Conditions`, xForData, y);
@@ -185,7 +187,45 @@ async function CreatePDF(formData) {
   y = 20;
   doc.setFontSize(40);
   const termsHeader = "Terms & Conditions";
-  doc.text(termsHeader, xForData, y);
+  doc.text(termsHeader, 10, y);
+  doc.line(10, y + 1, 13 + doc.getTextWidth(termsHeader), y + 1);
+
+  y = y + gapSpace * 2;
+  doc.setFontSize(14); // Smaller font size for better readability
+
+  const terms = [
+    "I acknowledge that I am over the age of 18.",
+    "I'm not under the influence of alcohol or drugs.",
+    "I have fully disclosed any physical and/or mental impairments such as, but not limited to: Psoriasis, Hepatitis, Diabetes.",
+    "I accept that getting a skin infection is a possibility as a result of receiving any tattoo procedure, particularly if I do not take proper care of my tattoo and follow my artist's aftercare instructions.",
+    "I acknowledge that it is not reasonably possible for the staff of Esoteric Tattoo to know whether or not I may have an allergic reaction to the pigments and processes used throughout the tattoo procedure and I accept that such a reaction is possible.",
+    "I understand that this tattoo is a permanent change to my body and no claims have been made regarding the ability to change or undo the work once I have consented to the design process and begun the tattoo.",
+    "I understand that using numbing creams for a tattoo procedure increases the potential to encounter adverse reactions such as but not limited to: anaesthetic poisoning, redness, swelling, tingling, prolonged period of numbness, shallow breathing.",
+    "I agree to release Esoteric Tattoo and its employees from any and all claims, damages and legal action arising from or connected in any way to the tattoo procedure. I indemnify and hold harmless Esoteric Tattoo against any claims, expenses, damages, and liabilities.",
+    "I have read all of the above information carefully and by signing this document I am acknowledging that I understand and agree to all the information stated within it.",
+    "I acknowledge that Esoteric Tattoo does not offer refunds.",
+  ];
+
+  doc.text("I understand and have fully disclosed the following:", xForData, y);
+  y = y + gapSpace;
+
+  terms.forEach((term, index) => {
+    const textLines = doc.splitTextToSize(term, 180);
+    doc.text(`${index + 1}. `, xForData, y);
+    textLines.forEach((line, lineIndex) => {
+      const yOffset = lineIndex * 6;
+      doc.text(`${line}`, xForData + 8, y + yOffset);
+    });
+    const lineSpacing = 8;
+    y = y + textLines.length * 4.5 + lineSpacing;
+  });
+
+  doc.text(
+    `I understand and have fully disclosed the following: ${formData.acknowledge}`,
+    xForData,
+    y
+  );
+  doc.addImage(formData.signatureImage, "PNG", 20, y, 100, 20);
 
   const pdfBuffer = doc.output("arraybuffer");
   const pdfBlob = new Blob([pdfBuffer], { type: "application/pdf" });
