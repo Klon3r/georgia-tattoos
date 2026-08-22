4b7b0ad9636cac1ecb6f91885733c5fd5a56a6f5
keiranbun <117739313+keiranbun@users.noreply.github.com>
2026-08-22 21:34:00 +1000

    chore: remove redundant vite/client-covered image module declarations

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,39 +1,9 @@
-declare module "*.gif" {
-  const src: string;
-  export default src;
-}
+/// <reference types="vite/client" />
 
 declare module "*.pdf" {
   const src: string;
   export default src;
 }
-
-declare module "*.jpg" {
-  const src: string;
-  export default src;
-}
-
-declare module "*.png" {
-  const src: string;
-  export default src;
-}
-
-declare module "*.PNG" {
-  const src: string;
-  export default src;
-}
-
-declare module "*.webp" {
-  const src: string;
-  export default src;
-}
-
-declare module "*.avif" {
-  const src: string;
-  export default src;
-}
-
-/// <reference types="vite/client" />
 
 interface ImportMetaEnv {
   readonly VITE_BOOKING_LOCALHOST: string;
